cd666d8dad01cef28a800ec21e489396e014df88
Jash <jashdpatel2511@gmail.com>
2025-09-24 09:53:39 +0530

    write code to handle multiple clients at serverSide

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import socket
+import threading
+
 ssocket = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
 host = socket.gethostname() 
 port  = 8000
@@ -10,9 +12,32 @@ ssocket.listen(5)
 print(f"server is ready to listen with size of 5 clint in client_quese")
 
 
-print("waiting to accept client")
-client,addr = ssocket.accept()
-print(f"Got a connection from {addr}")
+#print("waiting to accept client")
+#client,addr = ssocket.accept()
+#print(f"Got a connection from {addr}")
+
+def handle_client(client , addr) :
+    print(f"[NEW CONNECTION] {addr} connected ")
+
+    message = client.recv(1024).decode('utf-8')
+    print(f"[ {addr} ] says : {message} ")
+
+    clinet.send("Thanks! message recived").encode('utf-8')
+    
+    client.close()
+    print(f"[CONNECTION CLOSED] {addr} disconnected.")
+
+while True :
+    print("waiting to accept client")
+
+    client , addr = ssocket.accept()
+
+    print("connected to [{addr}]")
+    
+    thread = threading.Thread(target=handle_client , args=(client , addr))
+    print('assign new thread to handle [{addr}]')
+    
+    thread.start()
 
 
 
