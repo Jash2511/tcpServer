a9d3fdd1c1f3ffdfb9b91f45e8ab6a3e31d8d08c
Jash <jashdpatel2511@gmail.com>
2025-09-24 18:57:18 +0530

    write code so client can write message

--- client.py
+++ client.py
@@ -7,7 +7,9 @@ port = 8000
 csocket.connect((host,port)) 
 print(f"Socket is ready to connect at {host}:{port} ")
 
-csocket.send("hey we are oging to see new things".encode('utf-8'))
+message = input("Enter message : ") 
+
+csocket.send(message.encode('utf-8'))
 print('message send to server')
 
 message = csocket.recv(1024).decode('utf-8')
